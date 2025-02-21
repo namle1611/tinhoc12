1654294f1b8d10d6ec157330caaad513ec8a2863
namle1611 <shakydz123@gmail.com>
2025-02-21 10:32:18 +0700

    Update script.js

--- content/script.js
+++ content/script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = pword.value
-  if (u =="namle" && p=="shaky1234")
+  if (u =="namle" && p=="1234")
   {
     window.open("trangchu.html")
     document.getElementById(compiler).style.display="block"
