751217de2c8a5d0e2d90c73f42a52ca46333b28d
namle1611 <shakydz123@gmail.com>
2025-02-21 14:57:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+function login()
+
+{
+
+  let u = username.value
+
+  let p = pass.value
+
+  if (u=="m" && p=="1")
+
+  {
+
+    window.open("trangchu.html")
+
+    document.getElementById(compiler).style.display="block"
+
+  }
+
+  else
+
+  {
+
+    window.open("tryagain.html")
+
+  }
+
+}
