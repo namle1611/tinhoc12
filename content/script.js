8bf744d9e79918f3f4dfd66253934a31ac3667ab
namle1611 <shakydz123@gmail.com>
2025-02-21 10:16:55 +0700

    Create script.js

---
+++ content/script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let u = pword.value
+  if (u =="namle" && p=="shaky1234")
+  {
+    window.open("trangchu.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
