eb7e2b74c251b8eb572587826a4eadde84877713
namle1611 <shakydz123@gmail.com>
2025-02-21 10:31:58 +0700

    Update script.js

--- content/script.js
+++ content/script.js
@@ -7,7 +7,7 @@ function login()
     window.open("trangchu.html")
     document.getElementById(compiler).style.display="block"
   }
-  if (u !="namle" && p!="shaky1234")
+  if (u !="namle" && p!="1234")
   {
     window.open()
     document.getElementById(compiler).style.display="block"
